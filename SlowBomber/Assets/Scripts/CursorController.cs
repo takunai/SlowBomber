fabf3352cde4df178ba2c36f8c761637c60c9e7d
takuya <human.kagoshima.kurinotakuya@gmail.com>
2024-07-17 16:34:15 +0900

    [add]Asphalt,Sidewalk

---
+++ SlowBomber/Assets/Scripts/CursorController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Altキーを押している間、カーソルを表示してロック解除
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
